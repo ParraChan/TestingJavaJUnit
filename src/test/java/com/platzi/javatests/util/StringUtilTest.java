795cc2f2e6f0477c8c578e55c1f3647c7f0917e7
ParraChan <israel_marcial_parra@hotmail.com>
2022-04-21 17:06:19 -0500

    Unitary test with JUnit

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -1,25 +1,17 @@
 package com.platzi.javatests.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
 
-    public static void main(String[] args) {
-
-        assertEquals(StringUtil.repeat("hola",3),"holaholahola");
-      //  System.out.println(result);
+    @Test
+    public void testRepeat() {
 
 
-        String result2 = StringUtil.repeat("hola",1);
-     //   System.out.println(result2);
-        assertEquals(result2,"hola");
+        Assert.assertEquals("holaholahola",StringUtil.repeat("hola",3));
+        Assert.assertEquals("hola",StringUtil.repeat("hola",1));
 
 
     }
-
-    private static void assertEquals(String actual, String expected) {
-        if (!actual.equals(expected)){
-            //System.out.println("ERROR");
-            throw new RuntimeException(actual+" is not equal to expected "+ expected);
-        }
-    }
-
 }
