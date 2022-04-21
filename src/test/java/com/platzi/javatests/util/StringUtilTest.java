641290e5ba491609689f306ccf6d29a175ec2788
ParraChan <israel_marcial_parra@hotmail.com>
2022-04-21 18:01:13 -0500

    test with mockito to simulate a dice

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -3,15 +3,31 @@ package com.platzi.javatests.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThrows;
+
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-
-
-        Assert.assertEquals("holaholahola",StringUtil.repeat("hola",3));
+    public void repeat_string_once() {
         Assert.assertEquals("hola",StringUtil.repeat("hola",1));
+    }
+    @Test
+    public void repeat_string_multiple_times() {
+        Assert.assertEquals("holaholahola",StringUtil.repeat("hola",3));
+    }
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("",StringUtil.repeat("hola",0));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hola",-1);
+    }
 
-
+    @Test
+    public void repeat_string_negative_times2() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            StringUtil.repeat("hola", -1);
+        });
     }
 }
